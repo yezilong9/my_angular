7090ebc189311f028da937003e9c0364699ac7b0
Jero <ye363062816@qq.com>
2016-05-11 16:02:08 +0800

    multiply ng-app and bootstrap manually

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-angular.module("my_angular",["module1","module2"]);
+angular.module("my_angular",["module1"]);
+angular.module("my_angular_another",["module2"]);
+
+$(function(){
+  angular.bootstrap($("#another"),["my_angular_another"])
+})
